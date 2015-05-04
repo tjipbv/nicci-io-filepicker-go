ba05dbab8db2b523702ed68de867c7de5680bcbb
Pawel Knap <pawel@filepicker.io>
2015-05-04 10:21:41 +0200

    Add example for Stat method

--- filepicker/example_test.go
+++ filepicker/example_test.go
@@ -6,15 +6,14 @@ import (
 	"github.com/filepicker/filepicker-go/filepicker"
 )
 
+const ApiKey = "put_your_api_key_here"
+
 // This example shows how to send URLs content directly to your storage bucket.
 func ExampleStoreURL() {
-	const (
-		apiKey  = "put_your_api_key_here"
-		dataUrl = "https://d3urzlae3olibs.cloudfront.net/watermark.png"
-	)
+	const dataUrl = "https://d3urzlae3olibs.cloudfront.net/watermark.png"
 
 	// Create a new Filepicker.io client with S3 storage set by default.
-	cl := filepicker.NewClient(apiKey)
+	cl := filepicker.NewClient(ApiKey)
 
 	// Store URL content using default options.
 	blob, err := cl.StoreURL(dataUrl, nil)
@@ -28,11 +27,33 @@ func ExampleDownloadToFile() {
 	const fileHandle = "hFHUCB3iTxyMzseuWOgG"
 
 	// Create a new Filepicker.io client with S3 storage set by default.
-	cl := filepicker.NewClient(apiKey)
+	cl := filepicker.NewClient(ApiKey)
 
 	blob := filepicker.NewBlob(fileHandle)
 	if err := cl.DownloadToFile(blob, nil, "."); err != nil {
-		log.Fatalf("cannot download file with handle %s: %v\n", fileHandle, err)
+		log.Fatalf("cannot download file: %v\n", err)
 	}
-	log.Printf("file downloaded!")
+	log.Println("file downloaded!")
+}
+
+func ExampleStat() {
+	const fileHandle = "hFHUCB3iTxyMzseuWOgG"
+
+	// Create a new Filepicker.io client with S3 storage set by default.
+	cl := filepicker.NewClient(ApiKey)
+
+	options := &filepicker.MetaOpts{
+		Tags: []filepicker.MetaTag{filepicker.TagMd5Hash},
+	}
+
+	meta, err := cl.Stat(filepicker.NewHandle(fileHandle), options)
+	if err != nil {
+		log.Println("cannot stat file:", err)
+	}
+
+	md5hash := "unknown"
+	if md5, ok := meta.Md5Hash(); ok {
+		md5hash = md5
+	}
+	log.Println("MD5Hash:", md5hash)
 }
